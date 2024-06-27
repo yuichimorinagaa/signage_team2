fdf6b87aa7d014fa7d9fac36f684a335d3f5f0c2
gakutofurukawa <frkwgkt4918@keio.jp>
2024-06-27 22:40:22 +0900

    bladeの微妙な編集、画像の大きさ制限の突破、ルートの編集

--- laravel/resources/views/user/file.blade.php
+++ laravel/resources/views/user/file.blade.php
@@ -6,26 +6,26 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <!-- BootstrapのCSS読み込み -->
+    <link href="//../css/bootstrap.min.cssZone.Identifier" rel="stylesheet">
 </head>
 <body>
+<h2>画像選択画面</h2>
 <form action="{{route('file.store')}}" method="post" enctype="multipart/form-data">
     @csrf
     <input type="file" name="image" >
-    <input type="submit" value="投稿">
+    <input type="submit" class="btn btn-primary " value="投稿">
 </form>
+
 @foreach($image as $file)
-
-
     <div>
         <img src="{{ asset('storage/image/' . $file->file_path) }}" alt="画像の説明">
         <form action="{{ route('file.delete',['id'=>$file->id]) }}" method="POST">
             @csrf
             @method('DELETE')
-            <button type="submit">削除</button>
+            <button type="button" class="btn btn-primary ">削除</button>
         </form>
     </div>
-
-
 @endforeach
 
 </body>
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -26,5 +26,4 @@ Route::post('users/',[UserController::class,'store'])->name('user.store');
 
 Route::get('users/file',[FileController::class,'index'])->name('file.index');
 Route::post('users/file', [FileController::class,'store'])->name('file.store');
-
 Route::delete('users/file/{id}', [FileController::class, 'delete'])->name('file.delete');
