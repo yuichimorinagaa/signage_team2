7196498aa7805baa8f96767569041504c881eca9
gakutofurukawa <frkwgkt4918@keio.jp>
2024-06-30 12:03:21 +0900

    controllerとブレードの変更でバリデーションをつけています。コントローラにはコメントで過去のプログラムを消してありますが動作に問題がなければ消します。

--- laravel/app/Http/Controllers/FileController.php
+++ laravel/app/Http/Controllers/FileController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Model\File;
 use Illuminate\Support\Facades\Storage;
-use App\Http\Requests\FileUploadRequest;
+
 
 class FileController extends Controller
 {
