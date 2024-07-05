c33d1e83396d4e001808400b6dd85c396649891e
yugoinagaki <u5inagaki@gmail.com>
2024-07-05 20:59:45 +0900

    modelsファイルの修正

--- laravel/app/Models/Profile.php
+++ laravel/app/Models/Profile.php
@@ -9,5 +9,24 @@ class Profile extends Model
 {
     use HasFactory;
     protected $table = 'profiles';
-    protected $fillable = ['name'];
+    protected $fillable = [
+        'name',
+        'grade',
+        'university',
+        'profile_photo_path',
+        'joining_date',
+        'comment',
+        'hobbies',
+        'mbti',
+        'high_school',
+        'hometown',
+        'birthday',
+        'motto',
+        'restaurants',
+        'club_activities',
+        'famous_person',
+        'artists',
+        'if_ceo',
+        ];
+
 }
