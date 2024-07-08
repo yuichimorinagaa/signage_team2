05724342a7247d535c3bd9e1681579026dd726f8
gakutofurukawa <frkwgkt4918@keio.jp>
2024-07-08 16:28:14 +0900

    APIを現在時刻で取得に変更

--- laravel/resources/views/user/testApi.blade.php
+++ laravel/resources/views/user/testApi.blade.php
@@ -8,35 +8,11 @@
     <title>Document</title>
 </head>
 <body>
-<h1>天気情報</h1>
-
-@if(isset($weatherData))
-    <table>
-        <thead>
-        <tr>
-            <th>日時</th>
-            <th>天気</th>
-            <th>気温 (°C)</th>
-            <th>体感温度 (°C)</th>
-            <th>湿度 (%)</th>
-            <th>風速 (m/s)</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($weatherData as $data)
-            <tr>
-                <td>{{ \Carbon\Carbon::parse($data['datetime'])->format('Y-m-d H:i') }}</td>
-                <td>{{ $data['description'] }}</td>
-                <td>{{ $data['temperature'] }}</td>
-                <td>{{ $data['feels_like'] }}</td>
-                <td>{{ $data['humidity'] }}</td>
-                <td>{{ $data['wind_speed'] }}</td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
-@else
-    <p>天気情報を取得できませんでした。</p>
-@endif
+<h1>現在の天気情報</h1>
+<p><strong>天気:</strong> {{ $weather['description'] }}</p>
+<p><strong>気温:</strong> {{ $weather['temperature'] }} ℃</p>
+<p><strong>体感気温:</strong> {{ $weather['feels_like'] }} ℃</p>
+<p><strong>湿度:</strong> {{ $weather['humidity'] }}%</p>
+<p><strong>風速:</strong> {{ $weather['wind_speed'] }} m/s</p>
 </body>
 </html>
