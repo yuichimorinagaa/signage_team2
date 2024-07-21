aa363c52fa389e1182bf182d10d00c012ef0c245
YuichiMorinaga <yuichi10969@gmail.com>
2024-07-21 16:15:07 +0900

    管理者画面のデザインを整えました。 実際に使うときのことを考えて、ページネーションの実装、ユーザーを並び替える機能を作りました。

--- laravel/resources/views/admin/index.blade.php
+++ laravel/resources/views/admin/index.blade.php
@@ -1,97 +1,188 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
-    <style>
-        .user{
-            border-bottom: solid;
-        }
-        button {
-            margin: 5px;
-        }
-        .user-address{
-            font-size: 30px;
-        }
-    </style>
+    <title>ユーザー一覧</title>
+    <!-- BootstrapのCSS読み込み -->
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <!-- FontAwesomeの読み込み -->
+    <link href="https://use.fontawesome.com/releases/v6.5.2/css/all.css" rel="stylesheet">
 </head>
-<body>
-<h1>管理者画面</h1>
-<h3>ユーザー一覧</h3>
-@foreach($users as $user)
-    <div class="user">
-        <p class="user-address">{{$user->email}}</p>
-        <form action="{{route('admin.delete',['id'=>$user->id])}}" method="post">
-            @method('delete')
+<style>
+    body {
+        background-color: #F0F5F9;
+    }
+    header {
+        height: 45px;
+        width: 100%;
+        background-color: rgba(34, 49, 52, 0.9);
+    }
+    header p {
+        color: white;
+        font-size: 30px;
+        font-family: "Noto Serif", sans-serif;
+        font-weight: bold;
+        margin-left: 5px;
+    }
+    .header-left {
+        float: left;
+    }
+    .header-right {
+        float: right;
+        display: flex;
+    }
+    .logout{
+        height: 45px;
+        margin: 0;
+        background-color: rgba(255, 255, 255, 0.3);
+        transition: all 0.5s;
+        border: none;
+    }
+    .table-container {
+        margin: 20px;
+    }
+    .top-container{
+        display:flex;
+        justify-content: space-between;
+        padding:10px;
+    }
+    .top-container a {
+        margin-right: 10px;
+        text-decoration: none;
+        color: #007bff; /* Bootstrap のデフォルトリンク色 */
+        font-size: 1rem; /* フォントサイズを調整 */
+    }
+    .top-container a.active {
+        font-weight: bold;
+        text-decoration: underline;
+    }
+    .top-container .btn-link {
+        color: #007bff;
+        font-size: 1rem;
+    }
+    .top-container .btn-link.active {
+        font-weight: bold;
+        text-decoration: underline;
+    }
+
+</style>
+<header>
+    <div class="header-left">
+        <p>Admin</p>
+    </div>
+    <div class="header-right">
+        <form action="{{ route('logout') }}" method="POST">
             @csrf
-            <button type="button" data-toggle="modal" data-target="#deleteModal{{$user->id}}" class="btn btn-danger">
-                削除
+            <button type="submit" class="btn btn-danger logout">
+                <i class="fa-solid fa-sign-out-alt"></i> ログアウト
             </button>
-
-            <div class="modal fade" id="deleteModal{{$user->id}}" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel{{$user->id}}" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="deleteModalLabel{{$user->id}}">削除確認</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            本当にこのユーザーを削除しますか？
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">キャンセル</button>
-                            <form action="{{route('admin.delete',['id'=>$user->id])}}" method="post" style="display:inline;">
-                                @method('delete')
-                                @csrf
-                                <button type="submit" class="btn btn-danger">削除する</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
         </form>
-        <button type="button" data-toggle="modal" data-target="#imageModal{{$user->id}}" class="btn btn-danger">
+    </div>
+</header>
+<body>
+<div class="table-container">
+
+    <div class="top-container">
+        <div>
+            <a href="{{ route('admin.index', ['sort' => 'created_asc']) }}" class="btn btn-link {{ $sort == 'created_asc' ? 'active' : '' }}">登録順（昇順）</a>
+            <a href="{{ route('admin.index', ['sort' => 'created_desc']) }}" class="btn btn-link {{ $sort == 'created_desc' ? 'active' : '' }}">登録順（降順）</a>
+            <a href="{{ route('admin.index', ['sort' => 'email_asc']) }}" class="btn btn-link {{ $sort == 'email_asc' ? 'active' : '' }}">メールアドレス（昇順）</a>
+            <a href="{{ route('admin.index', ['sort' => 'email_desc']) }}" class="btn btn-link {{ $sort == 'email_desc' ? 'active' : '' }}">メールアドレス（降順）</a>
+        </div>
+        <button type="button" data-toggle="modal" data-target="#imageModal" class="btn btn-outline-danger delete-btn">
             画像を削除
         </button>
+    </div>
 
-        <div class="modal fade" id="imageModal{{$user->id}}" tabindex="-1" role="dialog" aria-labelledby="imageModalLabel{{$user->id}}" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="imageModalLabel{{$user->id}}">アップロードされた画像</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
+    <table class="table table-striped table-bordered">
+        <thead class="thead-dark">
+        <tr>
+            <th scope="col">ユーザー</th>
+            <th scope="col">操作</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($users as $user)
+            <tr>
+                <td>{{$user->email}}</td>
+                <td>
+                    <form action="{{route('admin.delete',['id'=>$user->id])}}" method="post" style="display:inline;">
+                        @method('delete')
+                        @csrf
+                        <button type="button" data-toggle="modal" data-target="#deleteModal{{$user->id}}" class="btn btn-outline-danger user-delete">
+                            削除
                         </button>
-                    </div>
-                    <div class="modal-body">
-                        @foreach($user->files as $file)
-                            <div>
-                                <img src="{{ url('storage/image/' . $file->file_path) }}" alt="User Image" style="max-width: 100%;">
-                                <form action="{{route('admin.deleteFile', ['id' => $file->id])}}" method="post">
-                                    @method('delete')
-                                    @csrf
-                                    <button type="submit" class="btn btn-danger mt-2">削除</button>
-                                </form>
+
+                        <!-- 削除確認モーダル -->
+                        <div class="modal fade" id="deleteModal{{$user->id}}" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel{{$user->id}}" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="deleteModalLabel{{$user->id}}">削除確認</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        本当にこのユーザーを削除しますか？
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">キャンセル</button>
+                                        <button type="submit" class="btn btn-danger">削除する</button>
+                                    </div>
+                                </div>
                             </div>
-                        @endforeach
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
-                    </div>
-                </div>
+                        </div>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+    <!-- ページネーションリンク -->
+    <div class="pagination-link">
+        {{$users->links('pagination::bootstrap-4')}}
+    </div>
+</div>
+
+
+
+<!-- すべての画像を表示するモーダル -->
+<div class="modal fade" id="imageModal" tabindex="-1" role="dialog" aria-labelledby="imageModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="imageModalLabel">アップロードされた画像</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                @foreach($users as $user)
+                    @foreach($user->files as $file)
+                        <div>
+                            <img src="{{ url('storage/image/' . $file->file_path) }}" alt="User Image" style="max-width: 100%;">
+                            <form action="{{route('admin.deleteFile', ['id' => $file->id])}}" method="post" style="display:inline;">
+                                @method('delete')
+                                @csrf
+                                <button type="submit" class="btn btn-danger mt-2">削除</button>
+                            </form>
+                        </div>
+                    @endforeach
+                @endforeach
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">閉じる</button>
             </div>
         </div>
     </div>
-@endforeach
-<form action="{{route('file.index')}}">
-    <button class="btn btn-primary">アップロード画面</button>
-</form>
+</div>
+
+<!-- BootstrapおよびjQueryのJS読み込み -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
