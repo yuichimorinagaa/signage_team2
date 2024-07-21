681753657bf706e879586a2f73a0e2603d17760b
YuichiMorinaga <yuichi10969@gmail.com>
2024-07-21 21:24:43 +0900

    自己紹介のフォームのデザイン

--- laravel/resources/views/profile/form.blade.php
+++ laravel/resources/views/profile/form.blade.php
@@ -4,11 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"> <!-- BootstrapのCSS読み込み -->
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
     <title>Document</title>
     <style>
         .form-group {
             margin: 10px 5px;
+        }
+        body{
+            background-color:#F0F5F9;
+        }
+        h1{
+            text-align:center;
+            padding-top:20px;
         }
     </style>
 </head>
